f724d9ab2cec74c85a6b8adc132d1bf3f8050248
Faiyaaz Khan <faiyaaz.khan13@gmail.com>
2023-06-02 11:48:41 +0200

    Add project files.

---
+++ CowAppBackend/Controllers/ScanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CowAppBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScanController : ControllerBase
+    {
+        // Endpoint that obtains 3 images and returns the result of the scan
+        [HttpPost]
+        public IActionResult Scan([FromForm] IFormFile[] images)
+        {
+            // generate random number
+            Random random = new ();
+            int randomNumber = random.Next(0, 2);
+
+            string result = randomNumber % 2 == 0 ? "positive" : "negative";
+
+            return Ok(result);
+        }
+    }
+}
